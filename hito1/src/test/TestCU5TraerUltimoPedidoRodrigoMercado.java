9727682f2140dbdff077f59f51f406cb6003378d
Matias Pacini <153960966+pacinimati@users.noreply.github.com>
2026-08-28 12:54:35 -0300

    borrar error duplicado

--- hito1/src/test/TestCU5TraerUltimoPedidoRodrigoMercado.java
+++ hito1/src/test/TestCU5TraerUltimoPedidoRodrigoMercado.java
@@ -1,29 +1,3 @@
-package test;
-
-import dao.PedidoDao;
-import datos.Pedido;
-
-public class TestCU5TraerUltimoPedidoRodrigoMercado {
-
-	public static void main(String[] args) {
-		
-		// TODO Auto-generated method stub
-		PedidoDao pedidoDao = new PedidoDao();
-		Pedido ultimo = pedidoDao.traerUltimoPedido();
-		
-        System.out.println("==================================================");
-        System.out.println("  CASO DE USO 5: TRAER EL ULTIMO PEDIDO REALIZADO ");
-        System.out.println("==================================================");
-
-		if (ultimo != null) {
-			System.out.println("ID Pedido: " + ultimo.getId());
-			System.out.println("Fecha de Transacción: " + ultimo.getFechaTransaccion());
-		} else {
-			System.out.println("No hay pedidos registrados en la base de datos.");
-		}
-	}
-
-}
 
 package test;
 
